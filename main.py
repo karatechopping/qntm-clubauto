10cd2863da3c80cd59a1147c24995b976bbcb866
Brett Kraiger <brett@marketingtech.pro>
2025-03-26 23:15:40 +1300

    go-live

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ import pytz
 
 # Define the mappings (Daxko -> GHL)
 field_mappings = {
-    # Standard Fields
     "FirstName": "firstName",
     "LastName": "lastName",
     "Email": "email",
@@ -20,7 +19,7 @@ field_mappings = {
         "phone",
         {
             "ghl_field": "cellPhone",
-            "ghl_id": "OzwZRpooGyR8Ayrc84u4"
+            "ghl_id": "gSKZNaZWJDQgSIyQwDMr"
         },
     ],
     "StreetAddress": "address1",
@@ -30,66 +29,63 @@ field_mappings = {
         "postalCode",
         {
             "ghl_field": "zip",
-            "ghl_id": "TAD26hyktLxWKZa9tOUS"
+            "ghl_id": "JRcvLEC9Z0d7rEFP6xDx"
         }
     ],
     "PhoneHome": {
         "ghl_field": "homePhone",
-        "ghl_id": "SCniAw3rl6DU6SH90OJi"
+        "ghl_id": "NADASHNYlJVbnkKV6DmL"
     },
     "PhoneWork": {
         "ghl_field": "workPhone",
-        "ghl_id": "vFOXH73DDi9WOabRLsNp"
+        "ghl_id": "lOA0OZrQQrcyyL6p6tWz"
     },
     "Gender": {
         "ghl_field": "what_is_your_gender",
-        "ghl_id": "O8Qy4nDKAtlUURXUiMHo"
+        "ghl_id": "rTpK9ZnqlUoegSl27lRt"
     },
-
-    # CA Fields
     "Status": {
         "ghl_field": "ca_status",
-        "ghl_id": "UB1ZC4lhT454zMArPudE"
+        "ghl_id": "PTRIDKO9282QhArQvQmx"
     },
     "SystemId": {
         "ghl_field": "ca_systemidnum",
-        "ghl_id": "HlknhU06tIHsPNe0wmqT"
+        "ghl_id": "tFugHrHNDMSAICLG7zQA"
     },
     "OptOutStatus": {
         "ghl_field": "optoutstatus",
-        "ghl_id": "7SXK1pY4CNzPyFndQOAv"
+        "ghl_id": "es6cJSYAXZM8326B0Z9X"
     },
     "DeliveryMethod": {
         "ghl_field": "deliverymethod",
-        "ghl_id": "NX3D1NYKLdDmOJYwGlgb"
+        "ghl_id": "lyckeY4wl04I9hVC3A9q"
     },
     "LastAPIUpdate": {
         "ghl_field": "last_api_update",
-        "ghl_id": "P6Yy2NTAwZjBJ6yaW1at"
+        "ghl_id": "DZXwU1OJbdBCzEXziB04"
     },
-
-    # Membership Types
-    "membership_type_1": {
+    "UserGroupName": {
         "ghl_field": "ca_membership_type_1",
-        "ghl_id": "wh6FNDp8VRuKRprDX7FS"
+        "ghl_id": "DxXkT3nt0Za5eFMCj0v2"
     },
     "membership_type_2": {
         "ghl_field": "ca_membership_type_2",
-        "ghl_id": "nQcW5wNd6k6h6afuF0MI"
+        "ghl_id": "yGRuRsmOYH2AJFcjMkRR"
     },
     "membership_type_3": {
         "ghl_field": "ca_membership_type_3",
-        "ghl_id": "BnmHQGkPRo67GXcseE5p"
+        "ghl_id": "lwrHOnrM56mKjXUbOGvF"
     },
     "membership_type_4": {
         "ghl_field": "ca_membership_type_4",
-        "ghl_id": "o5PWKwvqax6q3RvEBCLK"
+        "ghl_id": "vf5GH8ebre0UMYtBnzGA"
     },
     "membership_type_5": {
         "ghl_field": "ca_membership_type_5",
-        "ghl_id": "mTPYdH5By5gTOZgN9hHL"
+        "ghl_id": "0V5sPSxebwG0eQ5hbqYb"
     },
 }
+
 
 
 # Generate input_fields for fetcher
@@ -123,7 +119,7 @@ def main(run_csv=True, run_ghl=True, run_email=True, sample_size=-1):
 
 
     logger.info(f"Current ENV: SMTP_PASSWORD exists? {'SMTP_PASSWORD' in os.environ}")
-    logger.info(f"PYTHONPATH: {os.environ.get('PYTHONPATH')}") 
+    logger.info(f"PYTHONPATH: {os.environ.get('PYTHONPATH')}")
     logger.info(f"Current working directory: {os.getcwd()}")
 
     # Initialize components
